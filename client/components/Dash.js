5371e2ee9ed2576e70ffe5d39e0454c41d61ee33
Ona Narbutas <ona.narbutas@gmail.com>
2023-01-18 07:34:58 -0500

    Add basic fetch in Dash useEffect on initial render

--- client/components/Dash.js
+++ client/components/Dash.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ListContainer from "./ListContainer.js"
 import MapContainer from "./MapContainer.js";
 
-const Dash = () => {
+const Dash = (props) => {
+    
+    // on initial render, fetch safe locations based on either users current location if allowed or default location
+    useEffect(() => {
+        // Need to determine how to send default location with get request - header or query string?
+        fetch('/locations')
+            .then(response => response.json())
+            .then(response => props.setReceivedData(response));
+    }, [])
+
     return (
         <div className="dashboard">
             <MapContainer/>
