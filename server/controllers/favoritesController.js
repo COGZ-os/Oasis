f75b8b1ffe2815af27a84c5776b7157374d7c692
George M <gsmaroun@gmail.com>
2023-01-17 20:41:58 -0500

    added functions in auth, favorites, locations controllers

--- server/controllers/favoritesController.js
+++ server/controllers/favoritesController.js
@@ -1,22 +1,33 @@
-const db = require('../models/locationModels');
+const { Favorite } = require('../models/locationModels');
 
 const favoritesController = {
 
     getFavorites(req, res, next) {
+        
         res.locals.favorites = [];
         return next();
     },
     
-    addFavorite(req, res, next) {
-        res.locals.status = 'success';
-        return next();
+    async addFavorite(req, res, next) {
+        const { user_id, location_id } = req.body;
+        try {
+            const newFavoriteFromDB = await Favorite.create({ user_id, location_id });
+            res.locals.newFavorite = newFavoriteFromDB;
+            return next();
+        } catch (err) {
+            return next(err);
+        }
     },
-
-    removeFavorite(req, res, next) {
-        res.locals.status = 'success';
-        return next();
+    async removeFavorite(req, res, next) {
+        const { user_id, location_id } = req.body;
+        try {
+            const newFavoriteFromDB = await Favorite.destroy({ where: { user_id, location_id }});
+            res.locals.removedFavorite = newFavoriteFromDB;
+            return next();
+        } catch (err) {
+            return next(err);
+        }
     },
-
 }
 
 module.exports = favoritesController
--- server/controllers/locationsController.js
+++ server/controllers/locationsController.js
@@ -1,22 +1,119 @@
-const db = require('../models/locationModels');
+const { Location } = require('../models/locationModels');
+const { Op } = require('sequelize');
+const dotenv = require('dotenv');
+const { get } = require('../routes/authRouter');
+const { MAPS_API_KEY } = process.env;
+const apiKey = MAPS_API_KEY;
+
 
 const locationsController = {
 
-    getLocations(req, res, next) {
-        res.locals.locations = [];
-        return next();
+    async getLocations(req, res, next) {
+        const { search_type, lgbtq_category, location_category, name_pattern, address_pattern} = req.body;
+        let conditions;
+        if (search_type === 'lgbtq_category') {
+            conditions = {
+                lgbtq_category
+            }
+        } else if (search_type === 'location_category') {
+            conditions = {
+                location_category
+            }
+        } else if (search_type === 'name') {
+            conditions = {
+                name: {
+                    [Op.like]: `%${name_pattern}%`
+                  }
+            }
+        } else if (search_type === 'address') {
+            conditions = {
+                address_street: {
+                    [Op.like]: `%${address_pattern}%`
+                  }
+            }
+        } else {
+            conditions = {};
+        }
+        try {
+            const locations = await Location.findAll({
+                where: conditions
+            });
+            res.locals.locations = locations;
+            return next();
+        } catch (err) {
+            return next(err);
+        }
     },
     
-    createLocation(req, res, next) {
-        res.locals.newLocation = {'location': 0};
-        return next();
+    async createLocation(req, res, next) {
+        const {
+            user_id,
+            name,
+            location_category,
+            lgbtq_category,
+            address_street,
+            address_city,
+            address_state,
+            address_zipcode,
+            description,
+        } = req.body;
+        // '1600 Amphitheatre Parkway, Mountain View, CA'
+        const streetAddress = address_street + ', ' + address_city + ', ' + address_state;
+        const latLonObj = await getLonLatFromAddress(streetAddress);
+        const newLocation = {
+            user_id,
+            name,
+            location_category,
+            lgbtq_category,
+            address_street,
+            address_city,
+            address_state,
+            address_zipcode,
+            description,
+            safe_yes_votes: 0,
+            safe_no_votes: 0,
+            latitude: latLonObj.lat,
+            longitude: latLonObj.lng
+        };
+        try {
+            const newLocationFromDB = await Location.create(newLocation);
+            console.log(newLocationFromDB);
+            res.locals.newLocation = {'newlocation': newLocationFromDB};
+            return next();
+        } catch (err) {
+            return next(err);
+        }
     },
 
-    voteOnLocation(req, res, next) {
-        res.locals.updatedLocation = {'location': 1};
-        return next();
+    async voteOnLocation(req, res, next) {
+        const { id, method } = req.body;
+        try {
+            const location = await Location.findOne({ where: { id: id } });
+            let voteResult;
+            if (method === 'increment') {
+                voteResult = await location.increment(['safe_yes_votes'], { by: 1 });
+            } else if (method === 'decrement') {
+                voteResult = await location.decrement(['safe_no_votes'], { by: 1 });
+            }
+            res.locals.updatedLocation = {updatedLocation: voteResult};
+            return next();
+        } catch (err) {
+            return next(err);
+        }
     },
 
+}
+
+const getLonLatFromAddress = async (address) => {
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const location = data.results[0].geometry.location;
+        return location;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 module.exports = locationsController
