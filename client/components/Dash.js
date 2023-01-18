1a28115db42b1dae72cf64d50ae84d75054f43c6
Ona Narbutas <ona.narbutas@gmail.com>
2023-01-18 09:07:35 -0500

    Dash useEffect requests user location from browser

--- client/components/Dash.js
+++ client/components/Dash.js
@@ -6,6 +6,15 @@ const Dash = (props) => {
     
     // on initial render, fetch safe locations based on either users current location if allowed or default location
     useEffect(() => {
+        if ('geolocation' in navigator) {
+            console.log('geolocation available') ;
+            navigator.geolocation.getCurrentPosition(function(position) {
+                console.log(position.coords.latitude);
+                console.log(position.coords.longitude);
+            })
+        } else {
+            console.log('geolocation not available');
+        }
         // Need to determine how to send default location with get request - header or query string?
         fetch('/locations')
             .then(response => response.json())
