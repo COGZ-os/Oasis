4f40750141736e8406297dcd29abbbd1a1dbf80b
Zhaowei <sunzhaow0813@gmail.com>
2023-01-18 12:06:08 -0500

    Update

--- server/controllers/favoritesController.js
+++ server/controllers/favoritesController.js
@@ -1,11 +1,26 @@
-const { Favorite } = require('../models/locationModels');
+const { Favorite, Location } = require('../models/locationModels');
+const locationsController = require('./locationsController');
 
 const favoritesController = {
 
-    getFavorites(req, res, next) {
-        
-        res.locals.favorites = [];
-        return next();
+    async getFavorites(req, res, next) {
+        const { user_id } = req.params;
+        try {
+            const favoriteIds = await Favorite.findAll({ attributes: ['location_id'], where: { user_id }});
+            const favoriteLocations = await Location.findAll(
+                {
+                     where: {
+                        id: {
+                            [Sequelize.Op.in]: favoriteIds
+                        }
+                    }
+                }
+            );
+            res.locals.favorites = favoriteLocations;
+            return next();
+        } catch (err) {
+            return next(err);
+        }  
     },
     
     async addFavorite(req, res, next) {
